1c3d9e50c7574d8de69561c2dc379d51fb04cd25
takatronix <takatronix@gmail.com>
2020-02-19 16:53:16 +0900

    added mysql-connector

--- src/main/kotlin/red/man10/Man10CommonLibs.kt
+++ src/main/kotlin/red/man10/Man10CommonLibs.kt
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin
 class Man10CommonLibs : JavaPlugin(){
     override fun onEnable() {
         super.onEnable()
+        logger.info("Man10CommonLibs enabled")
     }
 
     override fun onDisable() {
