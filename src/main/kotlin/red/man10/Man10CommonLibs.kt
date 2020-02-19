39c4b65d761cece595bff375a9f1bbaa2432f394
forest611 <forest611328@gmail.com>
2020-02-19 15:58:49 +0900

    plugin.ymlなどのファイルをいくつか修正

---
+++ src/main/kotlin/red/man10/Man10CommonLibs.kt
@@ -0,0 +1,13 @@
+package red.man10
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10CommonLibs : JavaPlugin(){
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
